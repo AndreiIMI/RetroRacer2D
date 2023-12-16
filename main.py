5f12ca8ce99bd4d391acc1915d44d540e1853d0c
Imireanu Andrei <imireanuandrei@gmail.com>
2023-12-16 18:57:49 +0200

    add brake/deacceleration

--- main.py
+++ main.py
@@ -29,7 +29,10 @@ def main():
     z = 0
 
     road_pos = 0
-    road_acceleration = 80
+    road_velocity = 0
+    road_acceleration = 2  # Adjust the acceleration value
+    road_deacceleration = 1
+    road_brake = 5
     texture_position_acceleration = 4
     texture_position_threshold = 640
     half_texture_position_threshold = int(texture_position_threshold / 2)
@@ -44,9 +47,26 @@ def main():
 
         keys = pygame.key.get_pressed()
         if keys[K_UP]:
-            road_pos += road_acceleration
+            if road_velocity < 100:
+                road_velocity += road_acceleration
+            road_pos += road_velocity
             if road_pos >= texture_position_threshold:
                 road_pos = 0
+        elif keys[K_DOWN]:
+            if road_velocity > 0:
+                road_velocity -= road_brake
+            if road_velocity < 0:
+                road_velocity = 0
+            road_pos += road_velocity
+            if road_pos >= texture_position_threshold:
+                road_pos = 0
+        else:
+            if road_velocity > 0:
+                road_velocity -= road_deacceleration
+            road_pos += road_velocity
+            if road_pos >= texture_position_threshold:
+                road_pos = 0
+
 
         texture_position = road_pos
         dz = 0
@@ -65,8 +85,12 @@ def main():
 
             texture_position += texture_position_acceleration + z
 
-            if texture_position>=texture_position_threshold:
-               texture_position=0
+            if texture_position >= texture_position_threshold:
+                texture_position = 0
+
+        # Render the road_velocity variable on the top right of the screen
+        velocity_text = font.render(f"Speed: {road_velocity}", True, WHITE)
+        screen.blit(velocity_text, (SCREEN_WIDTH - velocity_text.get_width() - 10, 10))
 
         pygame.display.flip()
 
